7455edc2e6f1f8ee5664a9d27f8b2cfc626fc766
Shane <shane@shanessite.net>
2016-04-25 19:18:34 -0500

    Made the account information partial not need to hit the AWS endpoint

--- public/app/partials/userInfoPartialCtrl.js
+++ public/app/partials/userInfoPartialCtrl.js
@@ -3,12 +3,12 @@
 
 	var app = angular.module('tibialotteryApp').controller('UserInfoPartialController', UserInfoPartialController);
 
-	UserInfoPartialController.$inject = ['$rootScope', 'AccountService', '$sce'];
+	UserInfoPartialController.$inject = ['$rootScope', '$cookies', '$sce'];
 
-	function UserInfoPartialController($rootScope, AccountService, $sce) {
+	function UserInfoPartialController($rootScope, $cookies, $sce) {
 		var self = this;
 
-		self.loading = true;
+		self.loading = false; //Keeping this around in case we need to make this async again for some reason
 		self.loggedIn = false;
 		self.trustAsHtml = $sce.trustAsHtml;
 
@@ -17,16 +17,8 @@
 		$rootScope.$on('$stateChangeSuccess', refreshCurrengUserInformation); //Just in case we log in or out or something...
 
 		function refreshCurrengUserInformation(){
-			AccountService.getCurrentUserInformation(function (response) {
-				response = response.data;
-				self.loading = false;
-				if (!response.success) {
-					self.loggedIn = false;
-				}else{
-					self.loggedIn = true;
-					self.user = response.user;
-				}
-			});
+			self.user = $cookies.getObject('user');
+			self.loggedIn = !!self.user;
 		}
 
 		return self;
